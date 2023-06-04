81fca8eee0e139e161e4090974134999ba7a43a8
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-06-04 22:15:40 +0300

    there is tests that i could not pass

--- src/index.js
+++ src/index.js
@@ -4,3 +4,57 @@ import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// NavBar component
+const NavBar = () => {
+    const links = ['home', 'about', 'contact'];
+  
+    return (
+      <div>
+        {links.map((link, index) => (
+          <a key={index} href={`#${link}`}>
+            {link}
+          </a>
+        ))}
+      </div>
+    );
+  };
+  
+  // ProjectList component
+  const ProjectList = ({ projects }) => {
+    return (
+      <div>
+        {projects.map((project) => (
+          <ProjectItem key={project.id} technologies={project.technologies} />
+        ))}
+      </div>
+    );
+  };
+  
+  // ProjectItem component
+  const ProjectItem = ({ technologies }) => {
+    return (
+      <div>
+        {technologies.map((technology, index) => (
+          <span key={index}>{technology}</span>
+        ))}
+      </div>
+    );
+  };
+  
+  // App component
+  const App = () => {
+    const projects = [
+      { id: 1, technologies: ['React', 'JavaScript', 'HTML', 'CSS'] },
+      { id: 2, technologies: ['Node.js', 'Express', 'MongoDB'] },
+      { id: 3, technologies: ['Vue.js', 'TypeScript', 'SCSS'] },
+    ];
+  
+    return (
+      <div>
+        <NavBar />
+        <ProjectList projects={projects} />
+      </div>
+    );
+  };
+  
+  export default App;
